2f463505b6db57b3c26fbea9bcdd3dc0fcb46832
lgou2w <lgou2w@hotmail.com>
2021-05-01 20:21:25 +0800

    feat: basic completed

---
+++ src/api.ts
@@ -0,0 +1,211 @@
+import type * as types from './type'
+import type { Duplex } from 'stream'
+import { httpOverHttp, httpsOverHttp } from 'tunnel'
+import { v4 as uuidv4 } from 'uuid'
+import { createHmac } from 'crypto'
+import got from 'got'
+
+const debug = require('debug')('lgou2w:picacomic-api')
+const isDebug = typeof process.env.DEBUG !== 'undefined'
+
+export type Options = {
+  timeout: number
+  proxy: {
+    host: string
+    port: number
+  }
+  app: {
+    api: string
+    apiKey: string
+    signatureKey: string
+    accept: string
+    channel: '1' | '2' | '3'
+    version: string
+    uuid: string
+    platform: string
+    buildVersion: string
+    userAgent: string
+    imageQuality: 'original' | 'low' | 'medium' | 'high'
+  }
+}
+
+const DEFAULT_OPTION_APP: Options['app'] = {
+  api: 'https://picaapi.picacomic.com/',
+  apiKey: 'C69BAF41DA5ABD1FFEDC6D2FEA56B',
+  signatureKey: '~d}$Q7$eIni=V)9\\RK/P.RM4;9[7|@/CA}b~OW!3?EV`:<>M7pddUBL5n|0/*Cn',
+  accept: 'application/vnd.picacomic.com.v1+json',
+  channel: '2',
+  version: '2.2.1.2.3.3',
+  uuid: 'defaultUuid',
+  platform: 'android',
+  buildVersion: '44',
+  userAgent: 'okhttp/3.8.1',
+  imageQuality: 'original'
+}
+
+function mergeOptions (opts: Partial<Options>): Options {
+  const result: any = { ...opts }
+  mergeObjectProperty(result, DEFAULT_OPTION_APP, 'app')
+  return result
+}
+
+function mergeObjectProperty (src: any, value: any, property: string) {
+  if (!src[property]) src[property] = value
+  else for (const key in value) src[property][key] = value[key]
+}
+
+function makeAuthorizationHeaders (token: string) {
+  return { authorization: token }
+}
+
+type Response<T> = {
+  code: string
+  message: string
+  data: T
+}
+
+export class PicaComicAPI {
+  private readonly _fetch: typeof got
+  private readonly _opts: Options
+
+  constructor (opts?: Partial<Options>) {
+    opts = opts || {}
+    this._opts = mergeOptions(opts)
+    this._fetch = got.extend({
+      prefixUrl: this._opts.app.api,
+      maxRedirects: 0,
+      followRedirect: false,
+      timeout: opts.timeout,
+      retry: 0,
+      agent: opts.proxy
+        ? {
+            http: httpOverHttp({ proxy: opts.proxy }),
+            https: httpsOverHttp({ proxy: opts.proxy }) as any
+          }
+        : undefined,
+      hooks: {
+        beforeRequest: [
+          options => {
+            const url = options.url.toString()
+            const method = options.method
+            isDebug && debug('FETCH -> %s %s', method, url)
+            const {
+              api, apiKey, signatureKey,
+              channel, version, uuid, platform, buildVersion,
+              accept, userAgent, imageQuality
+            } = this._opts.app
+            const nonce = uuidv4().replace(/-/g, '')
+            const time = (Date.now() / 1000).toFixed(0)
+            const con = (
+              (url.charAt(0) === '/' ? url.substring(1) : url).replace(api, '') +
+              time + nonce + method + apiKey
+            ).toLowerCase()
+            const signature = createHmac('sha256', signatureKey).update(con).digest().toString('hex')
+            options.headers['content-type'] === 'application/json' && (options.headers['content-type'] = 'application/json; charset=UTF-8')
+            mergeObjectProperty(options, {
+              time,
+              nonce,
+              accept,
+              signature,
+              'api-key': apiKey,
+              'app-channel': channel,
+              'app-version': version,
+              'app-uuid': uuid,
+              'app-platform': platform,
+              'app-build-version': buildVersion,
+              'user-agent': userAgent,
+              'image-quality': imageQuality
+            }, 'headers')
+          }
+        ]
+      }
+    })
+  }
+
+  async signIn (payload: { email: string, password: string }): Promise<string> {
+    return this._fetch
+      .post('auth/sign-in', {
+        json: {
+          email: payload.email,
+          password: payload.password
+        }
+      })
+      .json<Response<{ token: string }>>()
+      .then(res => res.data.token)
+  }
+
+  async fetchCategories (payload: { token: string }): Promise<types.Category[]> {
+    return this._fetch
+      .get('categories', { headers: makeAuthorizationHeaders(payload.token) })
+      .json<Response<{ categories: types.Category[] }>>()
+      .then(res => res.data.categories)
+  }
+
+  async fetchComics (payload: { token: string, category: string, page?: number, sort?: 'ua' | 'dd' | 'da' | 'ld' | 'vd' }): Promise<types.Comics> {
+    return this._fetch
+      .get('comics', {
+        headers: makeAuthorizationHeaders(payload.token),
+        searchParams: {
+          c: payload.category,
+          page: payload.page || 1,
+          s: payload.sort || 'ua'
+        }
+      })
+      .json<Response<{ comics: types.Comics }>>()
+      .then(res => res.data.comics)
+  }
+
+  async fetchComic (payload: { token: string, id: string }): Promise<types.ComicInfo> {
+    return this._fetch
+      .get(`comics/${payload.id}`, { headers: makeAuthorizationHeaders(payload.token) })
+      .json<Response<{ comic: types.ComicInfo }>>()
+      .then(res => res.data.comic)
+  }
+
+  async fetchComicEpisodes (payload: { token: string, comicId: string, page?: number }): Promise<types.ComicEpisodes> {
+    return this._fetch
+      .get(`comics/${payload.comicId}/eps`, {
+        headers: makeAuthorizationHeaders(payload.token),
+        searchParams: {
+          page: payload.page || 1
+        }
+      })
+      .json<Response<{ eps: types.ComicEpisodes }>>()
+      .then(res => res.data.eps)
+  }
+
+  async fetchComicEpisodePages (payload: { token: string, comicId: string, epsOrder: number, page?: number }): Promise<types.ComicEpisodePages> {
+    return this._fetch
+      .get(`comics/${payload.comicId}/order/${payload.epsOrder}/pages`, {
+        headers: makeAuthorizationHeaders(payload.token),
+        searchParams: {
+          page: payload.page || 1
+        }
+      })
+      .json<Response<{ pages: types.ComicEpisodePages }>>()
+      .then(res => res.data.pages)
+  }
+
+  stringifyImageUrl (image: { fileServer: string, path: string }): string {
+    let fileServer = image.fileServer
+    let path = image.path
+    if (path.startsWith('tobeimg/')) {
+      fileServer = 'https://img.picacomic.com/'
+      path = path.substring(path.indexOf('/') + 1)
+    } else {
+      path = 'static/' + path
+    }
+    !fileServer.endsWith('/') && (fileServer = fileServer + '/')
+    path.charAt(0) === '/' && (path = path.substring(1))
+    return fileServer + path
+  }
+
+  async fetchImage (payload: { token: string, image: { fileServer: string, path: string } }): Promise<Duplex> {
+    const url = this.stringifyImageUrl(payload.image)
+    return this._fetch
+      .stream({
+        url,
+        headers: makeAuthorizationHeaders(payload.token)
+      })
+  }
+}
---
+++ src/type.ts
@@ -0,0 +1,94 @@
+/* eslint-disable camelcase */
+
+export interface Image {
+  originalName: string
+  path: string
+  fileServer: string
+}
+
+export interface Category {
+  _id: string
+  title: string
+  thumb: Image
+  isWeb?: boolean
+  active?: boolean
+  link?: string
+  description?: string
+}
+
+export interface Comic {
+  _id: string
+  id: string
+  title: string
+  author: string
+  totalViews: number
+  totalLikes: number
+  likesCount: number
+  pagesCount: number
+  epsCount: number
+  finished: boolean
+  categories: string[]
+  thumb: Image
+}
+
+export interface Paginate<T> {
+  docs: T
+  total: number
+  limit: number
+  page: number
+  pages: number
+}
+
+export interface Comics extends Paginate<Comic[]> {}
+
+export interface Creator {
+  _id: string
+  name: string
+  gender: string
+  verified: boolean
+  exp: number
+  level: number
+  characters: string[]
+  role: string
+  avatar: Image
+  title: string
+  slogan: string
+  character: string
+}
+
+export interface ComicInfo extends Comic {
+  _creator: Creator
+  chineseTeam: string
+  tags: string[]
+  updated_at: string
+  created_at: string
+  allowDownload: boolean
+  allowComment: boolean
+  isFavourite: boolean
+  isLiked: boolean
+  commentsCount: number
+}
+
+export interface ComicEpisode {
+  _id: string
+  id: string
+  title: string
+  order: number
+  updated_at: string
+}
+
+export interface ComicEpisodes extends Paginate<ComicEpisode[]> {}
+
+export interface ComicEpisodePage {
+  _id: string
+  id: string
+  media: Image
+}
+
+export interface ComicEpisodePages {
+  pages: Paginate<ComicEpisodePage[]>
+  ep: {
+    _id: string
+    title: string
+  }
+}
